7afb59bc0139c2a8bf59c9ce958367f6bfb6d3c7
= <babinsamrat123@gmail.com>
2025-05-17 09:23:49 +0530

    makes some changes

--- bin/config.dart
+++ bin/config.dart
@@ -3,11 +3,6 @@ import 'package:config_runner/config_runner.dart';
 
 
 void main(List<String> args) async {
-  if (args.isEmpty) {
-    log('Usage: dart run :generate <type>');
-    return;
-  }
-
   final type = args[0];
 
   switch (type) {
